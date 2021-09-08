abfcfae7f78fbb6b0933265c9fdeef39d276ccce
raTMole <ratmole@gmail.com>
2021-09-08 15:41:36 +0300

    simplified UI, fixed some bugs.No source reloads properly

--- obsArUcoSceneChanger.py
+++ obsArUcoSceneChanger.py
@@ -7,6 +7,7 @@ import glob
 import ffmpeg
 import threading
 import subprocess
+import multiprocessing
 import numpy as np
 import obspython as obs
 import cv2.aruco as aruco
@@ -19,21 +20,16 @@ global clone
 global sceneFrom
 global sceneTo
 global ArUcoDict
-global ScSourceArUco
-global newSourceName
-global newSourceResolution
-global process
+global sourceName
+global ProcessF
 
 def script_description():
-
     return "OBS ArUco Scene Switcher.\nClones and monitors a camera for ArUco markers.\nWhen a marker is detected, it switches to the specified scene.\n\nBy raTMole\nv0.1"
 
 def script_properties():
     devices = [f for f in glob.glob("/dev/video*")]
     devices.sort()
     ArUcoDicts = ['DICT_4X4_50','DICT_4X4_100','DICT_4X4_250','DICT_4X4_1000','DICT_5X5_50','DICT_5X5_100','DICT_5X5_250','DICT_5X5_1000','DICT_6X6_50','DICT_6X6_100','DICT_6X6_250','DICT_6X6_1000','DICT_7X7_50','DICT_7X7_100','DICT_7X7_250','DICT_7X7_1000','DICT_ARUCO_ORIGINAL','DICT_APRILTAG_16h5','DICT_APRILTAG_25h9','DICT_APRILTAG_36h10','DICT_APRILTAG_36h11']
-
-    Resolutions = ['None','16:9','16:10','4:1','1:1','1920x1080','1536x864','1440x810','1280x720','1152x648','1097x617','960x540','853x480','768x432','698x392','640x360']
 
     props = obs.obs_properties_create()
 
@@ -45,9 +41,7 @@ def script_properties():
     sceneFrom = obs.obs_properties_add_list(props, "sceneFrom", "Scene From", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
     sceneTo = obs.obs_properties_add_list(props, "sceneTo", "Scene To", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
         
-    obs.obs_properties_add_text(props, "newSourceName", "New Source Name", obs.OBS_TEXT_DEFAULT)
-    newSourceResolution = obs.obs_properties_add_list(props, "newSourceResolution", "New Source Resolution", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
-
+    obs.obs_properties_add_text(props, "sourceName", "Source Name", obs.OBS_TEXT_DEFAULT)
 
     for device in devices:
         obs.obs_property_list_add_string(source, device, device)
@@ -63,21 +57,44 @@ def script_properties():
         obs.obs_property_list_add_string(sceneFrom, name, name)
         obs.obs_property_list_add_string(sceneTo, name, name)
  
-    for Resolution in Resolutions:
-        obs.obs_property_list_add_string(newSourceResolution, Resolution, Resolution)
-           
     obs.source_list_release(scenes)
-
 
     return props
 
-    
-def script_unload():
-    global process
+def stop_ffmpeg():
+    pid = "/tmp/obsArUco.pid"
+    global ProcessF
+
     try:
-        process.communicate(str.encode("q")) #Equivalent to send a Q
+        ProcessF.communicate(str.encode("q")) #Equivalent to send a Q
     except:
-        obs.script_log(obs.LOG_INFO, "ffmpeg not running. Restarting")
+        if os.path.isfile(pid):
+            os.system("kill -9 `cat "+pid+"`")
+            os.system("rm "+pid)
+
+
+def start_ffmpeg(source,clone):
+    global ProcessF
+
+    pid = "/tmp/obsArUco.pid"
+
+    process = (
+        ffmpeg
+        .input(source, f="v4l2")
+        .output(clone, f="v4l2")
+        .global_args('-loglevel', 'quiet')
+        .overwrite_output()
+    )
+    ProcessF = process.run_async(pipe_stdin=True)        
+
+    pidfilename = os.path.join(pid)
+    pidfile = open(pidfilename, 'w')
+    pidfile.write(str(ProcessF.pid))
+    pidfile.close()
+
+
+def script_unload():
+    stop_ffmpeg()
 
 
 def script_load(settings):
@@ -86,18 +103,14 @@ def script_load(settings):
     global sceneFrom
     global sceneTo
     global ArUcoDict
-    global ScSourceArUco
-    global newSourceName
-    global newSourceResolution
+    global sourceName
 
     source              = obs.obs_data_get_string(settings, "source")
     clone               = obs.obs_data_get_string(settings, "clone")
     sceneFrom           = obs.obs_data_get_string(settings, "sceneFrom")
     sceneTo             = obs.obs_data_get_string(settings, "sceneTo")
     ArUcoDict           = obs.obs_data_get_string(settings, "ArUcoDict")
-    ScSourceArUco       = obs.obs_data_get_string(settings, "ScSourceArUco")
-    newSourceName       = obs.obs_data_get_string(settings, "newSourceName")
-    newSourceResolution = obs.obs_data_get_string(settings, "newSourceResolution")
+    sourceName          = obs.obs_data_get_string(settings, "sourceName")
 
 def script_update(settings):
     global source
@@ -105,18 +118,14 @@ def script_update(settings):
     global sceneFrom
     global sceneTo
     global ArUcoDict
-    global ScSourceArUco
-    global newSourceName
-    global newSourceResolution
+    global sourceName
 
     source              = obs.obs_data_get_string(settings, "source")
     clone               = obs.obs_data_get_string(settings, "clone")
     sceneFrom           = obs.obs_data_get_string(settings, "sceneFrom")
     sceneTo             = obs.obs_data_get_string(settings, "sceneTo")
     ArUcoDict           = obs.obs_data_get_string(settings, "ArUcoDict")
-    ScSourceArUco       = obs.obs_data_get_string(settings, "ScSourceArUco")
-    newSourceName       = obs.obs_data_get_string(settings, "newSourceName")
-    newSourceResolution = obs.obs_data_get_string(settings, "newSourceResolution")
+    sourceName          = obs.obs_data_get_string(settings, "sourceName")
 
 def set_current_scene(newName):
     scenes = obs.obs_frontend_get_scenes()
@@ -130,38 +139,20 @@ def module_loaded(module_name):
     grep_proc = subprocess.Popen(['grep', module_name], stdin=lsmod_proc.stdout)
     grep_proc.communicate()  # Block until finished
     return grep_proc.returncode == 0
-     
-   
-def create_ArUco_source():
 
-    global newSourceName
-    global newSourceResolution
+def updateSource():
+    global sourceName
 
     current_scene = obs.obs_frontend_get_current_scene()
     scene = obs.obs_scene_from_source(current_scene)
+    scene_item = obs.obs_scene_find_source(scene, sourceName)
+    sourceItem = obs.obs_sceneitem_get_source(scene_item)
 
-    settings = obs.obs_data_create()
-
-    ScSourceArUco = obs.obs_source_create_private("v4l2_input", newSourceName, settings)
-    obs.obs_data_set_string(settings, "resolution", newSourceResolution)
-
+    settings = obs.obs_source_get_settings(sourceItem)
     obs.obs_data_set_string(settings, "device_id", clone )
-    obs.obs_data_set_bool(settings, "auto_reset", True )
-    obs.obs_data_set_int(settings, "timeout_frames", 50 )
-    obs.obs_data_set_bool(settings, "buffering", False);
-    ScSourceArUco = obs.obs_source_create_private("v4l2_input", newSourceName, settings)
-    obs.obs_data_set_string(settings, "resolution", newSourceResolution)
-    scale = obs.obs_source_create_private("scale_filter", "Scale", settings)
-    obs.obs_source_filter_add(ScSourceArUco, scale)   
-    
-    obs.obs_scene_add(scene, ScSourceArUco)
+    obs.obs_source_update(sourceItem, settings)
+    obs.obs_source_update(sourceItem, settings)
     obs.obs_data_release(settings)
-    obs.obs_source_release(ScSourceArUco)
-      
-    obs.obs_scene_release(scene)
-    scene_item = obs.obs_scene_find_source(scene, newSourceName)
-    obs.obs_sceneitem_set_order(scene_item,obs.OBS_ORDER_MOVE_BOTTOM)
-
 
 def findArucoMarkers(frame):
     global count
@@ -179,7 +170,6 @@ def findArucoMarkers(frame):
     arucoParam = aruco.DetectorParameters_create()
     corners, ids, rejected = aruco.detectMarkers(img_gray, arucoDict, parameters = arucoParam)
     draw=True
-
 
     if draw:
         if np.all(ids is not None):  # If there are markers found by detector
@@ -200,10 +190,8 @@ def run():
     global sceneFrom
     global sceneTo
     global ArUcoDict
-    global ScSourceArUco
-    global newSourceName
-    global newSourceResolution
-    global process
+    global sourceName
+    global ProcessF
 
     if not module_loaded('v4l2loopback'):
         obs.script_log(obs.LOG_ERROR, "v4l2loopback module missing!\nScript will not work!\nPlease install v4l2loopback module and run:\nsudo modprobe v4l2loopback devices=2")
@@ -221,43 +209,30 @@ def run():
         obs.script_log(obs.LOG_WARNING, "Please set ArUco Dictionary and reload the script")
         return
 
-    if not newSourceName:
-        obs.script_log(obs.LOG_WARNING, "Please set New Source Name and reload the script")
+    if not sourceName:
+        obs.script_log(obs.LOG_WARNING, "Please set Source Name and reload the script")
         return
 
-    if not newSourceResolution:
-        obs.script_log(obs.LOG_WARNING, "Please set New Source Resolution and reload the script")
-        return
+    stop_ffmpeg()
 
-    process = (
-        ffmpeg
-        .input(source, f="v4l2")
-        .output(clone, f="v4l2")
-        .global_args('-loglevel', 'quiet')
-        .overwrite_output()
-    )
-
-
-    process = process.run_async(pipe_stdin=True)        
-
+    start_ffmpeg(source,clone)
+    
     cam = cv2.VideoCapture(clone)
     cv2_version_major = int(cv2.__version__.split('.')[0])
 
     set_current_scene(sceneFrom)
-    create_ArUco_source()
+    updateSource()
 
     while True:
-
         ret, frame = cam.read()
         if not ret:
             obs.script_log(obs.LOG_ERROR, "Failed to grab frame! If you have changed the Source or Video Device, please restart OBS")
             break
-                    
         findArucoMarkers(frame)
 
     cam.release()
     cv2.destroyAllWindows()
 
-x = threading.Thread(target=run, args=())
+x = threading.Thread(target=run, args=(),daemon = True)
 x.start()
 
